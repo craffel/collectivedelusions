904b78556dd22a3533ffcea9f373b07394de567d
Colin Raffel <craffel@gmail.com>
2026-06-26 02:44:55 +0000

    Clean up test files

--- images/test_flux_load.py
+++
@@ -1,6 +0,0 @@
-import torch
-from diffusers import FluxPipeline
-print("Loading pipe")
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.2-dev", torch_dtype=torch.bfloat16)
-pipe = pipe.to("cuda:0")
-print("Loaded on cuda:0")
